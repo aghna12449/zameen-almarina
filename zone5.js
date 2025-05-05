ad982a39a3328dd7fd2081cada66ad72b77f185d
aghna12449 <aghna.faran@dubizzlelabs.com>
2025-05-05 20:07:45 +0500

    dynamic zone 3 and 1

---
+++ zone5.js
@@ -0,0 +1,3 @@
+const zone5hotspotXML = `
+<hotspot name="hs101" style="mypolygonalhotspotstylename" > <point ath="-0.16920461092695405" atv="-0.057383399931835374" /> <point ath="-0.15329471282882423" atv="-0.06445447838760662" /> <point ath="-0.1432184339359992" atv="-0.05791374610952532" /> <point ath="-0.1571837919165091" atv="-0.05172655550503635" /> </hotspot>
+`
