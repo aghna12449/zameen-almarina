10f13ad92006585ccc1c01e51996ab4b36101c9a
aghna12449 <aghna.faran@dubizzlelabs.com>
2025-07-23 18:02:15 +0500

    Fixed Amenties

---
+++ src/zoneAmenities.js
@@ -0,0 +1,60 @@
+const zoneAmenities = {
+  'ZONE 1': [
+    {  label: 'Park', parkTime: '2 mins' },
+    {  label: 'Mosque', mosqueTime: '2 mins' },
+    { label: 'Shopping', mallTime: '7 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '4 mins' }
+  ],
+  'Zone 2' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '2 mins' },
+    {  label: 'Shopping', mallTime: '6 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '4 mins' }
+  ],
+   'ZONE 3' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '2 mins' },
+    {  label: 'Shopping', mallTime: '3 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '5 mins' }
+  ],
+   'ZONE 4' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '2 mins' },
+    {  label: 'Shopping', mallTime: '1 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '6 mins' }
+  ],
+   'ZONE 5' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '4 mins' },
+    {  label: 'Shopping', mallTime: '3 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '8 mins' }
+  ],
+   'ZONE 6' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '3 mins' },
+    {  label: 'Shopping', mallTime: '3 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '7 mins' }
+  ],
+   'ZONE 7' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '2 mins' },
+    {  label: 'Shopping', mallTime: '5 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '5 mins' }
+  ],
+   'ZONE 8' : [
+    {  label: 'Park', parkTime: '2 min' },
+    { label: 'Mosque', mosqueTime: '2 mins' },
+    {  label: 'Shopping', mallTime: '6 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '3 mins' }
+  ],
+   'ZONE 9' : [
+    {  label: 'Park', parkTime: '1 min' },
+    { label: 'Mosque', mosqueTime: '2 mins' },
+    {  label: 'Shopping', mallTime: '10 mins' },
+    {  label: 'Pharmacy', pharmacyTime: '1 mins' }
+  ],
+  
+};
+
+
+window.zoneAmenities = zoneAmenities;
